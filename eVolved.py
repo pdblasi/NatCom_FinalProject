8f810ea1f751c2a2b04d43e20c7f7db402faa35b
unknown <1955241@T-SMD1016331.SDSMT.local>
2015-05-06 17:30:59 -0600

    Fixed the modules. They didn't have __init__.py's so I guess they didn't count...

--- eVolved.py
+++ eVolved.py
@@ -1,11 +1,11 @@
 from Tkinter import *
-from App import *
+from App import App
 
 def main():
     root = Tk()
     root.geometry("500x500")
     root.resizable(width=False, height=False)
-    app = App(root)
+    app = App.App(root)
     root.mainloop()
 
 if __name__ == '__main__':
